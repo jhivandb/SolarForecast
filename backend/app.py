44604a611e7887e5b8dd985dfcd420b9f658fe31
Jhivan <jhivanb@gmail.com>
2024-04-08 16:16:34 +0530

    Added cors

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,21 @@
 from flask import Flask, jsonify
+from flask_cors import CORS,cross_origin
 from pymongo import MongoClient
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 # MongoDB connection string. Replace it with your connection string if you're using MongoDB Atlas or a different setup.
 mongo_uri = "mongodb+srv://jhivanb:25may2001@solarforecast.lh6oebd.mongodb.net/"
 client = MongoClient(mongo_uri)
+
 
 # Replace 'your_db_name' with your actual database name and 'your_collection_name' with your collection's name.
 db = client['solarforecast']
 collection = db['plant1']
 
 @app.route('/solar', methods=['GET'])
+@cross_origin()
 def get_solar():
     solar_data_point = list(collection.find({}, {'_id': 0})) 
     modified_items = []
